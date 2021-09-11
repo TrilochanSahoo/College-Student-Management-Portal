5a21136048dc928a331ae85e1abc844ba7dcbc2e
TrilochanSahoo <71021905+TrilochanSahoo@users.noreply.github.com>
2021-09-11 23:25:25 +0530

    adding index page

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const path = require("path")
 const app = express()
 
 require("./db/conn")
 
 const port = process.env.PORT || 3000
+const static_path = path.join(__dirname,"../public")
+
+app.use(express.static(static_path))
 
 app.get('/',(req, res)=>{
     res.send(`Hello to Home page`)
