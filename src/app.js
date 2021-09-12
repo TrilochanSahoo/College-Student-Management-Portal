9045ef9dfa4aab7ab7fe1be494d34bba10a04ec1
TrilochanSahoo <71021905+TrilochanSahoo@users.noreply.github.com>
2021-09-12 12:03:06 +0530

    update : adding hbs and partials

--- src/app.js
+++ src/app.js
@@ -1,16 +1,26 @@
 const express = require('express')
 const path = require("path")
+const hbs = require('hbs')
 const app = express()
 
 require("./db/conn")
 
 const port = process.env.PORT || 3000
+
+// path 
 const static_path = path.join(__dirname,"../public")
+const view_path = path.join(__dirname,"../templates/views")
+const partials_path = path.join(__dirname,"../templates/partials")
+
+app.set("view engine", "hbs")
+app.set("views", view_path)
+hbs.registerPartials(partials_path)
 
 app.use(express.static(static_path))
 
+// routing 
 app.get('/',(req, res)=>{
-    res.send(`Hello to Home page`)
+    res.render("index")
 })
 
 app.get('/courses',(req, res)=>{
@@ -22,11 +32,12 @@ app.get('/staffs',(req, res)=>{
 })
 
 app.get('/login',(req, res)=>{
-    res.send(`Hello to Login page`)
+    res.render("login")
 })
 
-
-console.log(`subscribe`)
+app.get('*',(req, res)=>{
+    res.render("404error")
+})
 
 app.listen(port,()=>{
     console.log(`server is running`)
